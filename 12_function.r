4ed43197fd7084c8abea8851282271b1814f68e9
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-12-13 21:29:20 +0700

    update 12_function.r

--- 12_function.r
+++ 12_function.r
@@ -3,3 +3,28 @@ helloFun <- function(name) {
 }
 
 helloFun("wury")
+
+# tanpa return keyword, ekspresi terakhir otomatis akan menjadi return
+sum <- function(x, y) {
+    x + y
+}
+
+res <- sum(5, 5)
+print(res)
+
+# dengan return keyword secara eksplisit, sama seperti diatas
+sumWithReturnKeyword <- function(x, y) {
+    return (x + y)
+}
+
+print(sumWithReturnKeyword(5,5))
+
+# fungsi dengan multipel return value
+myFunction <- function() {
+    person <- list("id" = 101, "name" = "Wuriyanto")
+    return (person)
+}
+
+p <- myFunction()
+print(p$id)
+print(p$name)
